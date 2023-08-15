e004539182362f86a3f579f2b79c6677183ce948
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2023-08-15 16:11:39 -0500

    added option for api and deleted unsed code

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -1,11 +1,3 @@
-// document.addEventListener("DOMContentLoaded", ()=>{
-//   noteTitle = document.querySelector('.note-title');
-//   noteText = document.querySelector('.note-textarea');
-//   saveNoteBtn = document.querySelector('.save-note');
-//   newNoteBtn = document.querySelector('.new-note');
-//   noteList = document.querySelectorAll('.list-container .list-group');
-// })
-
 // Declare variables to store DOM elements
 let noteTitle;
 let noteText;
--- Develop/public/server.js
+++ Develop/public/server.js
@@ -1,7 +1,29 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const PORT = 3000;
 const app = express();
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/api/notes", (req, res) => {
+  // Read the content of the db.json file and send it as a response
+  const dbFilePath = path.join(__dirname, '../db/db.json');
+  fs.readFile(dbFilePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error("Error reading db.json:", err);
+      return res.status(500).send("Internal Server Error");
+    }
+
+    try {
+      const notes = JSON.parse(data);
+      res.json(notes);
+    } catch (parseError) {
+      console.error("Error parsing db.json:", parseError);
+      return res.status(500).send("Internal Server Error");
+    }
+  });
+});
 
 // Serve static files from the current directory (public)
 app.use(express.static(__dirname));
