73d6d1fb26509737e7a338c53f745a3e491f2107
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2023-08-16 18:20:19 -0500

    fixed api notes not showing on the sidebar

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const noteData = require('../db/db.json')
+
 const PORT = process.env.port || 3000;
 const app = express();
-// Middleware for parsing JSON and urlencoded form data
-app.use(express.urlencoded({ extended: true }));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 // Serve static files from the current directory (public)
 app.use(express.static(__dirname));
 
@@ -17,58 +18,11 @@ app.get("/", (req, res) => {
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, 'notes.html'));
 });
-app.get("/api/notes", (req, res) => {
-  // Read the content of the db.json file and send it as a response
-  const dbFilePath = path.join(__dirname, '../db/db.json');
-  fs.readFile(dbFilePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error("Error reading db.json:", err);
-      return res.status(500).send("Internal Server Error");
-    }
 
-    try {
-      const notes = JSON.parse(data);
-      res.json(notes);
-    } catch (parseError) {
-      console.error("Error parsing db.json:", parseError);
-      return res.status(500).send("Internal Server Error");
-    }
-  });
-});
-app.post("/api/notes", (req, res) => {
-  const newNote = req.body;
-
-  // Read the existing notes from the db.json file
-  const dbFilePath = path.join(__dirname, '../db/db.json');
-  fs.readFile(dbFilePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error("Error reading db.json:", err);
-      return res.status(500).send("Internal Server Error");
-    }
-
-    try {
-      const notes = JSON.parse(data);
-
-      // Assign a unique ID to the new note
-      newNote.id = Date.now();
-
-      // Add the new note to the array of notes
-      notes.push(newNote);
-
-      // Write the updated notes array back to the db.json file
-      fs.writeFile(dbFilePath, JSON.stringify(notes, null, 2), (writeErr) => {
-        if (writeErr) {
-          console.error("Error writing to db.json:", writeErr);
-          return res.status(500).send("Internal Server Error");
-        }
-
-        res.json(newNote); // Respond with the new note
-      });
-    } catch (parseError) {
-      console.error("Error parsing db.json:", parseError);
-      return res.status(500).send("Internal Server Error");
-    }
-  });
+// API routes
+app.get('/api/notes', (req, res) => {
+  const notes = JSON.parse(fs.readFileSync(path.join(__dirname, '../db/db.json'), 'utf8'));
+  res.json(notes);
 });
 
 app.listen(PORT, () => {
