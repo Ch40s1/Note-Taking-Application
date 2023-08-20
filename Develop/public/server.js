e1106f93e2326d1c45fb98aaac8cc8a883e638a9
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2023-08-20 13:05:59 -0500

    initial commit

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -1,20 +1,25 @@
+// get all the required tools needed to make the application work
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const noteData = require('../db/db.json')
 
+// make a express app and define the port used
 const PORT = process.env.port || 3000;
 const app = express();
 
+// middleware to create a safety barrier agasint injections
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve static files from the current directory (public)
 app.use(express.static(__dirname));
 
+// This is for the index.html.
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// this loads the notes.html
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, 'notes.html'));
 });
@@ -30,22 +35,31 @@ app.get('/api/notes', (req, res) => {
 
 // Post allows us to make send the notes to the servers and add it to the file system
 app.post('/api/notes', (req, res) => {
+  // create a variable for the request
   const newNote = req.body;
+  // reads the file and parses it
   const notes = JSON.parse(fs.readFileSync(path.join(__dirname, '../db/db.json'), 'utf8'));
+  // adds an id to the note
   newNote.id = notes.length.toString(); // Assign a simple incremental ID
+  // pushes the note to the array
   notes.push(newNote);
+  // essentially re-writes the file adding all the new content
   fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(notes));
   res.json(newNote);
 });
+// allows the deletion of notes.
 app.delete('/api/notes/:id', (req, res) => {
+  // checks id
   const id = req.params.id;
+  // reads the file and parses it
   const notes = JSON.parse(fs.readFileSync(path.join(__dirname, '../db/db.json'), 'utf8'));
+  // look for the note with the id
   const updatedNotes = notes.filter(note => note.id !== id);
   fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(updatedNotes));
   res.json({ message: 'Note deleted successfully' });
 });
 
-
+// listens for the server/port
 app.listen(PORT, () => {
   console.log(`Server live at http://localhost:${PORT}`);
 });
