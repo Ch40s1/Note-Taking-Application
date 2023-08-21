b45095679f00d1fb51395a24b1be4f9d581986dc
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2023-08-21 18:05:51 -0500

    heroku error h10 and h14 fixed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const noteData = require('./Develop/db/db.json')
+const noteData = require('./db/db.json')
 
 // make a express app and define the port used
 const PORT = process.env.port || 3000;
@@ -12,23 +12,23 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve static files from the current directory (public)
-app.use(express.static(path.join('./Develop/public')));
+app.use(express.static(path.join('./public')));
 
 // This is for the index.html.
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 // this loads the notes.html
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+  res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 // API routes
 //this is the get route for the nptes api
 app.get('/api/notes', (req, res) => {
   // uses the file system to parse the content of the db.json and assigns it to a constant
-  const notes = JSON.parse(fs.readFileSync(path.join('./Develop/db/db.json'), 'utf8'));
+  const notes = JSON.parse(fs.readFileSync(path.join('./db/db.json'), 'utf8'));
   // responds with the notes
   res.json(notes);
 });
@@ -38,13 +38,13 @@ app.post('/api/notes', (req, res) => {
   // create a variable for the request
   const newNote = req.body;
   // reads the file and parses it
-  const notes = JSON.parse(fs.readFileSync(path.join('./Develop/db/db.json'), 'utf8'));
+  const notes = JSON.parse(fs.readFileSync(path.join('./db/db.json'), 'utf8'));
   // adds an id to the note
   newNote.id = notes.length.toString(); // Assign a simple incremental ID
   // pushes the note to the array
   notes.push(newNote);
   // essentially re-writes the file adding all the new content
-  fs.writeFileSync(path.join('./Develop/db/db.json'), JSON.stringify(notes));
+  fs.writeFileSync(path.join('./db/db.json'), JSON.stringify(notes));
   res.json(newNote);
 });
 // allows the deletion of notes.
@@ -52,10 +52,10 @@ app.delete('/api/notes/:id', (req, res) => {
   // checks id
   const id = req.params.id;
   // reads the file and parses it
-  const notes = JSON.parse(fs.readFileSync(path.join('./Develop/db/db.json'), 'utf8'));
+  const notes = JSON.parse(fs.readFileSync(path.join('./db/db.json'), 'utf8'));
   // look for the note with the id
   const updatedNotes = notes.filter(note => note.id !== id);
-  fs.writeFileSync(path.join('./Develop/db/db.json'), JSON.stringify(updatedNotes));
+  fs.writeFileSync(path.join('./db/db.json'), JSON.stringify(updatedNotes));
   res.json({ message: 'Note deleted successfully' });
 });
 
